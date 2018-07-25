2d9373fec382c8fa8f9b411eaceadc22a1a52228
BADAGUE <youness.badague@hpe.com>
2018-07-25 11:38:59 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { UserService } from './user.service';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
+declare var $;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+  data: any;
+  interval: any;
+  nb:number=0;
+  getauth=false;
+  alertError=false;
+  username:any;
+  password:any;
+  constructor(private http:Http, private user:UserService, private router:Router ){
+    
+  }
+
+  ngOnInit() {
+    this.alertError=false;
+      if(this.user.getUserLoggedIn()==true){
+          this.getauth=true;
+      }
+  }
+  getAuth(){
+    return this.getauth;
+  }
+  Logged(e: NgForm){
+    if(e.value['username']=="admin" && e.value['password']=="admin"){
+      this.getauth=false;
+      this.user.setUserLoggedIn();
+      this.alertError=false;
+      $('#loginModal').modal('hide');
+      this.getauth=true;
+      this.router.navigate(['adminGui']);
+    }
+    else{
+      this.alertError=true;
+    }
+   
+  }
+  tab3par(){
+    document.getElementById("3par").click();
+  }
+  dd(){
+    document.getElementById("dd").click();
+  }
+
+  emcvmax(){
+    document.getElementById("menu2").click();
+  }
+
+  storeonce(){
+    document.getElementById("storeonce").click();
+  }
+
+  brocade(){
+    document.getElementById("brocade").click();
+  }
+
+  cisco(){
+    document.getElementById("cisco").click();
+  }
+
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,43 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PannesComponent } from './pannes/pannes.component';
 
+import { Routes, RouterModule } from "@angular/router";
+import { Panne3parComponent } from './panne3par/panne3par.component';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import {NgxPaginationModule} from 'ngx-pagination';
+import { UserService } from './user.service';
+import { AdminGuiComponent } from './admin-gui/admin-gui.component';
+import { AuthguardGuard } from './authguard.guard';
 
-@NgModule({
+const routes:Routes=[
+  { path:'pannes', component:PannesComponent},
+  { path:'3par', component:Panne3parComponent},
+  { path:'',component:PannesComponent},
+  {
+    path:'adminGui', canActivate:[AuthguardGuard], component: AdminGuiComponent
+  }
+ 
+]
+
+@NgModule({ 
   declarations: [
-    AppComponent
+    AppComponent,
+    PannesComponent,
+    Panne3parComponent,
+    AdminGuiComponent,
+    
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(routes), HttpModule, FormsModule, NgxPaginationModule
   ],
-  providers: [],
+  providers: [UserService,AuthguardGuard],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {}
+
+
